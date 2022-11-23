431b0dc7510d9a3787e670529457070fca863f59
Camilo Castro <camilo.castrotech@gmail.com>
2022-11-23 14:29:28 -0500

    api rest usuarios

---
+++ src/main/java/com/example/appsmarthhouse/AppSmarthhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.appsmarthhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSmarthhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSmarthhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appsmarthhouse/AppSmarthhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appsmarthhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSmarthhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
